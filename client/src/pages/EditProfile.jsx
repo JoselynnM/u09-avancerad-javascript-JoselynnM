f7f5a176d40fc840618168c129952850a781167a
Joselyn <joselyn.moreta@chasacademy.se>
2023-11-07 19:05:01 +0100

    reponsive profl

--- client/src/pages/EditProfile.jsx
+++ client/src/pages/EditProfile.jsx
@@ -49,87 +49,85 @@ const EditProfile = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bg-gray-200 p-8 rounded-lg shadow-lg text-gray-700 max-w-md w-full">
-        <h1 className="text-2xl font-bold text-center text-teal-600 mb-6">
-          Edit Your Profile
-        </h1>
-        <div className="flex justify-center">
-          {!profilePicture ? (
-            <img
-              className="h-16 w-16 rounded-full object-cover"
-              src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
-              alt="Profile Image"
-            />
-          ) : (
-            <img
-              className="h-16 w-16 rounded-full object-cover"
-              src={profilePicture}
-              alt="Profile Image"
-            />
-          )}
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="profilePicture"
-            className="block text-sm text-gray-500"
-          >
-            Upload Profile Picture
-          </label>
-          <input
-            type="file"
-            id="profilePicture"
-            accept="image/*"
-            onChange={handleProfilePictureChange}
-            className="bg-gray-100 border-2 border-gray-400 text-gray-700 p-2 rounded w-full"
+    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="bg-gray-200 p-8 rounded-lg shadow-lg text-gray-700 max-w-md w-full">
+      <h1 className="text-2xl font-bold text-center text-teal-600 mb-6">
+        Edit Your Profile
+      </h1>
+      <div className="flex justify-center">
+        {!profilePicture ? (
+          <img
+            className="h-16 w-16 rounded-full object-cover"
+            src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
+            alt="Profile Image"
           />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="username" className="block text-sm text-gray-500">
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            placeholder="Your Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="bg-gray-100 border-2 border-gray-400 text-gray-700 p-2 rounded w-full"
+        ) : (
+          <img
+            className="h-16 w-16 rounded-full object-cover"
+            src={profilePicture}
+            alt="Profile Image"
           />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-sm text-gray-500">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Your Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-100 border-2 border-gray-400 text-gray-700 p-2 rounded w-full"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="bio" className="block text-sm text-gray-500">
-            Bio
-          </label>
-          <textarea
-            id="bio"
-            placeholder="Tell us about yourself"
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
-            className="bg-gray-100 border-2 border-gray-400 text-gray-700 p-2 rounded w-full"
-          />
-        </div>
-        <button
-          onClick={handleUpdateProfile}
-          className="w-full bg-teal-600 text-white text-sm font-medium py-2 rounded hover:bg-teal-700"
-        >
-          SAVE
-        </button>
+        )}
       </div>
+      <div className="mb-4">
+        <label htmlFor="profilePicture" className="block text-sm text-gray-500">
+          Upload Profile Picture
+        </label>
+        <input
+          type="file"
+          id="profilePicture"
+          accept="image/*"
+          onChange={handleProfilePictureChange}
+          className="bg-gray-100 border-2 border-gray-400 text-gray-700 p-2 rounded w-full"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="username" className="block text-sm text-gray-500">
+          Username
+        </label>
+        <input
+          type="text"
+          id="username"
+          placeholder="Your Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="bg-gray-100 border-2 border-gray-400 text-gray-700 p-2 rounded w-full"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="password" className="block text-sm text-gray-500">
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Your Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="bg-gray-100 border-2 border-gray-400 text-gray-700 p-2 rounded w-full"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="bio" className="block text-sm text-gray-500">
+          Bio
+        </label>
+        <textarea
+          id="bio"
+          placeholder="Tell us about yourself"
+          value={bio}
+          onChange={(e) => setBio(e.target.value)}
+          className="bg-gray-100 border-2 border-gray-400 text-gray-700 p-2 rounded w-full"
+        />
+      </div>
+      <button
+        onClick={handleUpdateProfile}
+        className="w-full bg-teal-600 text-white text-sm font-medium py-2 rounded hover:bg-teal-700"
+      >
+        SAVE
+      </button>
     </div>
+  </div>
+  
   );
 };
 
--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -61,74 +61,66 @@ function UserProfile() {
 
   return (
     <>
-      <div className="flex flex-col items-center sm:pt-10 pt-4">
-        <div className="w-24 h-24 relative mx-auto mb-4">
-          {!profilePicture ? (
-            <img
-              className="w-full h-full object-cover rounded-full"
-              src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
-              alt="Profile Image"
-            />
-          ) : (
-            <img
-              className="w-full h-full object-cover rounded-full"
-              src={profilePicture}
-              alt="Profile Image"
-            />
-          )}
-        </div>
-        <div className="text-center">
-          {username === storedUsername && (
-            <Link
-              to={`/home/edit-profile/${storedUsername}`}
-              className="bg-gray-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded text-sm"
-            >
-              Edit Profile
-            </Link>
-          )}
-        </div>
+     <div className="flex flex-col items-center sm:pt-10 pt-4">
+  <div className="w-24 h-24 relative mx-auto mb-4">
+    {!profilePicture ? (
+      <img
+        className="w-full h-full object-cover rounded-full"
+        src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png"
+        alt="Profile Image"
+      />
+    ) : (
+      <img
+        className="w-full h-full object-cover rounded-full"
+        src={profilePicture}
+        alt="Profile Image"
+      />
+    )}
+  </div>
+  <div className="text-center">
+    {username === storedUsername && (
+      <Link
+        to={`/home/edit-profile/${storedUsername}`}
+        className="bg-gray-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded text-sm"
+      >
+        Edit Profile
+      </Link>
+    )}
+  </div>
 
-        <div className="text-center ">
-          <h5 className="text-xl font-semibold mt-3">{username}</h5>
-          <p className="text-gray-600">{bio}</p>
-        </div>
+  <div className="text-center">
+    <h5 className="text-xl font-semibold mt-3">{username}</h5>
+    <p className="text-gray-600">{bio}</p>
+  </div>
 
-        <div className="flex flex-col justify-evenly  items-center shadow-xl rounded-lg w-[600px] m-20 gap- p-9 bg-white">
-          <h1 className="text-4xl font-bold">Quiz Results</h1>
+  <div className="flex flex-col justify-evenly items-center shadow-xl rounded-lg sm:w-2/3 lg:w-1/2 mx-4 mt-10 p-6 bg-white">
+    <h1 className="text-4xl font-bold">Quiz Results</h1>
 
-          <table className="w-full mt-5">
-            <thead className="bg-gray-100">
-              <tr>
-                <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 uppercase">
-                  Answer
-                </th>
-                <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 uppercase">
-                  Correct answer
-                </th>
-                <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 uppercase">
-                  Your answer
-                </th>
-              </tr>
-            </thead>
-            {/* <tbody className='divide-y divide-gray-200'>
-          {results.map((result, index) => (
-            <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
-              <td className='px-6 py-4 whitespace-nowrap'>{result.question}</td>
-              <td className='px-6 py-4 whitespace-nowrap'>{result.correctAnswer}</td>
-              <td className='px-6 py-4 whitespace-nowrap'>{result.yourAnswer}</td>
-            </tr>
-          ))}
-        </tbody> */}
-          </table>
+    <table className="w-full mt-5">
+      <thead className="bg-gray-100">
+        <tr>
+          <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 uppercase">
+            Answer
+          </th>
+          <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 uppercase">
+            Correct answer
+          </th>
+          <th className="px-6 py-3 text-left text-sm font-medium text-gray-600 uppercase">
+            Your answer
+          </th>
+        </tr>
+      </thead>
+      {/* Aquí puedes agregar las filas de tu tabla */}
+    </table>
 
-          <button
-            className="border px-5 py-2 mt-5 rounded-lg transition-all font-bold hover:bg-yellow-500 hover:text-gray-900"
-            // onClick={onReset}
-          >
-            Try again
-          </button>
-        </div>
-      </div>
+    <button
+      className="border px-5 py-2 mt-5 rounded-lg transition-all font-bold hover:bg-yellow-500 hover:text-gray-900"
+    >
+      Try again
+    </button>
+  </div>
+</div>
+
     </>
   );
 }
