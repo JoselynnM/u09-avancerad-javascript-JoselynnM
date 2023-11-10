36a98428c623e5540c9be379140c0b607ab6a035
Joselyn <joselyn.moreta@chasacademy.se>
2023-11-10 17:47:34 +0100

    edit

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -197,7 +197,7 @@ const Search = () => {
         <div className="relative flex-1 w-full lg:w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
           <div className="p-6">
             <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-              1. City Driving
+              8. City Driving
             </h5>
             <img
               src="/images/citycar.jpeg"
